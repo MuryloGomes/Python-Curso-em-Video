0169654797e1c0748fb6f4f8a5891fac7cae21b1
Murylo Gomes Alves <muryloalves15@gmail.com>
2024-08-27 19:35:29 -0300

    ex096

---
+++ exercicios/096.py
@@ -0,0 +1,8 @@
+def area(largura, comprimento):
+    area_do_terreno = largura * comprimento
+    print(f'A área de um terreno {largura} x {comprimento} é de {area_do_terreno}m.')
+
+largura = float(input('LARGURA (m): '))
+comprimento = float(input('COMPRIMENTO (m): '))
+
+area(largura, comprimento)
