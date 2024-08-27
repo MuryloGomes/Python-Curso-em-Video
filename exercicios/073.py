32d1b103080a6a15e12a306fa78eea283ebc17e8
Murylo Gomes Alves <muryloalves15@gmail.com>
2024-08-27 16:10:34 -0300

    ex073

---
+++ exercicios/073.py
@@ -0,0 +1,9 @@
+times = ('corinthians', 'palmeiras', 'santos', 'gremio', 'cruzeiro', 'flamengo', 'vasco da gama', 'chapecoense', 'atletico-mg', 'botafogo', 'athletico-pr', 'bahia', 'são paulo', 'fluminense', 'sport recife', 'vitoria', 'coritiba', 'avai', 'ponte preta', 'atletico-go')
+
+print(times[0:5])
+
+print(times[15:])
+
+print(sorted(times))
+
+print(times.index('chapecoense'))
