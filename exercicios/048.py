663ca2a48f0f70a47e0adcafb60382d931402031
Murylo Gomes Alves <muryloalves15@gmail.com>
2024-08-29 13:34:54 -0300

    ex048.v2

--- exercicios/048.py
+++ exercicios/048.py
@@ -1,7 +1,7 @@
 variavel_acumuladora = 0
 
 for c in range(1, 501):
-    if c % 3 == 0:
+    if c % 3 == 0 and c % 2 != 0:
         variavel_acumuladora += c
 
 print(f'A soma de todos os números impares multiplos de 3 é {variavel_acumuladora}')
